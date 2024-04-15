f91ed8c2edee62abb8627ee2d9337c52e8401890
tobias van der vaart <113106074+TobiasVdvaart@users.noreply.github.com>
2024-04-15 11:24:09 +0200

    vote systeem// anti error

--- examplebot.py
+++ examplebot.py
@@ -1,6 +1,4 @@
 import discord
-
-# Local imports
 from settings import DISCORD_API_SECRET
 
 intents = discord.Intents.default()
@@ -17,12 +15,21 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('$hello wizard'):
         await message.channel.send('Hello!')
     elif message.content.startswith('$goodbye'):
         await message.channel.send('Goodbye!')
-
-    if message.content.startswith('$help'):
-        await message.channel.send('How can I assist you?')
+    elif message.content.startswith('$wizard help'):
+        await message.channel.send('How can the great wizard assist you?')
+    elif message.content.startswith('$vote'):
+        question = message.content[len('$vote'):].strip()
+        if question:
+            vote_message = await message.channel.send(f"Vote: {question}")
+            await vote_message.add_reaction('👍')
+            await vote_message.add_reaction('👎')
+        else:
+            await message.channel.send("Please provide a question to vote on using '$vote <question>'.")
+    elif message.content == '$':
+        await message.channel.send('Sorry, I do not understand that command.')
 
 client.run(DISCORD_API_SECRET)
